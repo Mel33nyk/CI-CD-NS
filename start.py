13c941b93595ac530b61abfb06ff12acc310d675
Nazar Melnyk <nazar228@rivne.itstep.org>
2020-09-06 16:44:24 +0300

    Refractor19

--- start.py
+++ start.py
@@ -1,16 +1,11 @@
-import requests
+from lib.Currency import Currency
+import time
 
-URL =  "https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5"   
+currency = Currency()
 
-def get_data(URL):
-    responce = requests.get(URL)
-    data = responce.json()
-    show_currencies(data)
-
-def show_currencies(currency):
-    for item in currency:
-        print(item["ccy"] + " " + item["base_ccy"] +
-        " " + item["buy"] + " | " + item["sale"])
-
-
-get_data(URL)
+counter = 0
+while True:
+    counter+=1
+    print("Get online data => ", counter, " times")
+    currency.start()
+    time.sleep(300)
